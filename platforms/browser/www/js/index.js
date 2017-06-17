957fd7bc0835510e877d1f28eb52daf798fde36a
markoars <marko.arsovski@gmail.com>
2017-06-17 21:55:04 +0200

    Test 4

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -45,6 +45,9 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 */
+
+        googleMapApi.drawMapInitial();
+
         console.log('Received Event: ' + id);
     }
 
@@ -56,13 +59,13 @@ var app = {
 };
 
 
-
+// ############################ Camera ############################
 var cameraApi = {
 
 
     // ############################ Camera #################################
     takePicture: function() {
-        alert("camera");
+       // alert("camera");
       navigator.camera.getPicture( function( imageURI ) {
         alert( imageURI );
       },
@@ -83,6 +86,178 @@ var cameraApi = {
 var locationApi = {
 
 
+    getLocation: function(){
+
+        alert("start");
+        navigator.geolocation.getCurrentPosition(locationApi.onGetLocationSuccess, locationApi.onGetLocationError);
+        alert("end");
+    },
+
+    onGetLocationSuccess: function(position){
+
+        alert("index.js - success");
+        googleMapApi.drawMap(position);
+
+        webApi.sendCoordinatesToServer(position);
+    },
+    
+    onGetLocationError: function(error){
+        alert("index.js - the code is " + error.code + ". \n" + "message: " + error.message);
+
+        switch(error.code)
+        {
+            case error.PERMISSION_DENIED: alert("User did not share geolocation data");break;  
+            case error.POSITION_UNAVAILABLE: alert("Could not detect current position");break;  
+            case error.TIMEOUT: alert("Retrieving position timed out");break;  
+            default: alert("Unknown Error");break;  
+        }
+    },
+
+
+
+    init_WatchLocation: function() {  
+
+            $("#initWatch").toggleClass("active");
+            $("#stopWatch").toggleClass("active");
+
+
+            if(watchProcess == null){
+            alert("watch start");
+            watchProcess = navigator.geolocation.watchPosition(locationApi.watchLocationSuccess, locationApi.watchLocationError);
+            }
+    },
+
+    stop_WatchLocation: function() { 
+
+            $("#initWatch").toggleClass("active");
+            $("#stopWatch").toggleClass("active");
+
+            if(watchProcess != null){
+            navigator.geolocation.clearWatch(watchProcess);
+            watchProcess = null;
+            alert("index.js - watch stopped");
+            }
+    },
+
+    watchLocationSuccess: function(position){
+            alert("index.js - handle success");
+            googleMapApi.drawMap(position);
+    },
+
+     watchLocationError: function(error){
+        
+        switch(error.code)
+        {
+            case error.PERMISSION_DENIED: alert("User did not share geolocation data");break;  
+            case error.POSITION_UNAVAILABLE: alert("Could not detect current position");break;  
+            case error.TIMEOUT: alert("Retrieving position timed out");break;  
+            default: alert("Unknown Error");break;  
+        }
+    },
+
+    
+};
+
+
+// ############################ Map ############################
+var googleMapApi = {
+
+
+    drawMapInitial: function() {
+
+            var mapContainer = $('#googleMap');
+
+            var myLatLong = new google.maps.LatLng(41.9961439,21.4318572); // Skopje
+
+            var mapProp= {
+                center: myLatLong,
+                zoom:16,
+                draggable: false ,
+                streetViewControl: false ,
+                // mapTypeControl: false,
+                disableDoubleClickZoom: true,
+                scrollwheel: false,
+                backgroundColor: '#f2efe9',
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(mapContainer[0],mapProp);
+
+           // var marker = new google.maps.Marker({ position: myLatLong, map:map });
+    },
+
+
+    drawMap: function(position) {
+
+           // alert("index.js - google map Api- draw poi lat: " + position.coords.latitude);
+
+           // var mapContainer = $('#googleMap');
+
+            var myLatLong = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+          /*  var mapProp= {
+                center: myLatLong,
+                zoom:16,
+                draggable: false ,
+                streetViewControl: false ,
+                // mapTypeControl: false,
+                disableDoubleClickZoom: true,
+                scrollwheel: false,
+                backgroundColor: '#f2efe9',
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };*/
+            //var map=new google.maps.Map(mapContainer[0],mapProp);
+
+            //var marker = new google.maps.Marker({ position: myLatLong, map:map });
+            //alert("1");
+            //googleMapApi.clearMarkers();
+            googleMapApi.addMarker(myLatLong);
+    },
+
+
+    drawStaticMap: function(position) {
+            var container = $('#googleMap');
+            var imageUrl = "https://maps.googleapis.com/maps/api/staticmap?sensor=false&center=" + position.coords.latitude + "," +  
+                        position.coords.longitude + "&zoom=18&size=640x500" + "&key=AIzaSyCXWhC15jIOu95QuaTBvRHYkc6Npi0HBvw";  
+
+            //https://maps.googleapis.com/maps/api/staticmap?center=52.47433501832194,9.891406250000045&zoom=18&size=640x500&markers=color:blue|label:S|52.47433501832194,9.891406250000045&key=AIzaSyCXWhC15jIOu95QuaTBvRHYkc6Npi0HBvw
+            //https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY        &zoom=13&size=600x300&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=YOUR_API_KEY
+
+            $('<img/>',{
+            src : imageUrl
+            }).appendTo(container);
+    },
+
+
+    // Sets the map on all markers in the array.
+      setMapOnAll: function (map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      },
+
+      // Removes the markers from the map, but keeps them in the array.
+      clearMarkers: function() {
+        //alert("2");
+        setMapOnAll(null);
+      },
+
+      // Adds a marker to the map and push to the array.
+      addMarker: function (location) {
+         // alert("add marker");
+        var marker = new google.maps.Marker({
+          position: location,
+          map: map
+        });
+        markers.push(marker);
+      }
+
+};
+
+
+// ############################ Web ############################
+var webApi = {
+
+
     sendDummyLocation: function(){
 
 
@@ -97,141 +272,23 @@ var locationApi = {
         alert("end dummy");
     },
 
-    getLocation: function(){
 
+    sendCoordinatesToServer: function(position){
 
-        alert("start");
-        navigator.geolocation.getCurrentPosition(locationApi.onGeoLocationSuccess, locationApi.onGeoLocationError);
-        alert("end");
-    },
+            var longitude = position.coords.longitude;
+            var latitude = position.coords.latitude;
 
-    onGeoLocationSuccess: function(position){
+            alert("index.js - send coordinates to Server enter");
 
-        var longitude = position.coords.longitude;
-        var latitude = position.coords.latitude;
-
-        alert("success");
-        locationApi.drawMap(position);
-
-        // Send coordinates to server
-        $.getJSON('http://maxbet.mk.azhar.arvixe.com/api/products/1/' + longitude + '/' + latitude + '/')
-                .done(function (data) {
-                    alert("sucessfully inserted coordinate to server long: " + longitude + " lat: " + latitude);
-                })
-                .fail(function (jqXHR, textStatus, err) {
-                        alert("fail gps 1 " + textStatus);
-                 });
-
-    },
-    
-    onGeoLocationError: function(error){
-        alert("the code is " + error.code + ". \n" + "message: " + error.message);
-
-        switch(error.code)
-        {
-            case error.PERMISSION_DENIED: alert("user did not share geolocation data");
-            break;
-
-            case error.POSITION_UNAVAILABLE: alert("could not detect current position");
-            break;
-
-            case error.TIMEOUT: alert("retrieving position timed out");
-            break;
-
-            default: alert("unknown error");
-            break;
-        }
-    },
-
-
-
-
-
-    initiate_watchlocation: function() {  
-
-
-        if(!$("#initWatch").hasClass("active"))
-        {
-            $("#initWatch").addClass("active");
-            $("#stopWatch").removeClass("active");
-        }
-        
-        
-
-        if(watchProcess == null){
-        alert("watch process starting");
-        watchProcess = navigator.geolocation.watchPosition(locationApi.watchLocationSuccess,locationApi.watchLocationError);
-        }
-    },
-
-    stop_watchlocation: function() { 
-
-
-        if(!$("#stopWatch").hasClass("active"))
-        {
-            $("#stopWatch").addClass("active");
-            $("#initWatch").removeClass("active");
-        }
-
-        if(watchProcess != null){
-        alert("watch process stopping");
-        navigator.geolocation.clearWatch(watchProcess);
-        }
-    },
-
-    watchLocationSuccess: function(position){
-            //alert("handle success");
-            locationApi.drawMap(position);
-    },
-
-     watchLocationError: function(error){
-        
-        switch(error.code)
-        {
-            case error.PERMISSION_DENIED: alert("User did not share geolocation data");break;  
-            case error.POSITION_UNAVAILABLE: alert("Could not detect current position");break;  
-            case error.TIMEOUT: alert("Retrieving position timed out");break;  
-            default: alert("Unknown Error");break;  
-        }
-    },
-
-    drawMap: function(position) {
-            alert("draw poi lat: " + position.coords.latitude);
-
-            var container = $('#map_canvas');
-            //var zoomLevel = $('#zoomLevel');
-
-            alert("draw poi 1");
-            var myLatLong = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-
-
-            alert("draw poi 2");
-            var mapOptions = {  center: myLatLong,
-                                zoom: 19,
-                                mapTypeId: google.maps.MapTypeId.ROADMAP
-                                };
-        
-
-            alert("draw poi 3");
-            var map = new google.maps.Map(container[0],mapOptions);
-
-
-            alert("draw poi 4");
-            container.css('display','block');
-
-
-            alert("draw poi 5"); 
-            var marker = new google.maps.Marker({ 
-            position: myLatLong,
-            map:map,
-            title:"My Position (Accuracy of Position: " + position.coords.accuracy + " Meters), Altitude: " 
-                + position.coords.altitude + ' Altitude Accuracy: ' + position.coords.altitudeAccuracy
-            });
-
-            alert("draw poi end");
+            // Send coordinates to server
+            $.getJSON('http://maxbet.mk.azhar.arvixe.com/api/products/1/' + longitude + '/' + latitude + '/')
+                    .done(function (data) {
+                        alert("sucessfully inserted coordinate to server long: " + longitude + " lat: " + latitude);
+                    })
+                    .fail(function (jqXHR, textStatus, err) {
+                            alert("fail gps 1 " + textStatus);
+                    });
 
     }
 
-
-    
 };
